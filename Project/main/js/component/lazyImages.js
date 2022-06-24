613bba186edd23426f209176eb407dc53556a36c
Denidelito <59993452+Denidelito@users.noreply.github.com>
2022-06-24 17:49:35 +0300

    Добавил прелоудер для изображений, изменил конфиг eslinter

---
+++ Project/main/js/component/lazyImages.js
@@ -0,0 +1,38 @@
+const lazyImages = document.querySelectorAll('img[data-src],source[srcset]');
+const windowHeight = document.documentElement.clientHeight;
+
+const lazyImagesPosition = [];
+
+if (lazyImages.length > 0) {
+    lazyImages.forEach((img) => {
+        if (img.dataset.src || img.dataset.srcset) {
+            lazyImagesPosition.push(img.getBoundingClientRect().top + pageYOffset);
+            lazyScrollCheck();
+        }
+    });
+}
+
+window.addEventListener('scroll', lazyScroll);
+
+/** Описание функции* */
+function lazyScroll() {
+    if (document.querySelectorAll('img[data-src], source[srcset]').length > 0) {
+        lazyScrollCheck();
+    }
+}
+
+/** Проверка картинки на видимость* */
+function lazyScrollCheck() {
+    const imgIndex = lazyImagesPosition.findIndex((item) => pageYOffset > item - windowHeight);
+    if (imgIndex >= 0) {
+        if (lazyImages[imgIndex].dataset.src) {
+            lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
+            lazyImages[imgIndex].removeAttribute('data-src');
+        } else if (lazyImages[imgIndex].dataset.srcset) {
+            lazyImages[imgIndex].srcset = lazyImages[imgIndex].dataset.srcset;
+            lazyImages[imgIndex].removeAttribute('data-srcset');
+        }
+
+        delete lazyImagesPosition[imgIndex]
+    }
+}
